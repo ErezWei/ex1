5925b0e8b346ddb200f3f0b8622fe395b0876126
ErezWei <erez.weinfeld@live.biu.ac.il>
2024-11-18 12:45:56 +0200

    all 5 tasks completed

--- ex1.c
+++ ex1.c
@@ -57,12 +57,28 @@ int main() {
   printf("\nEven - Odd:\n");
   /* Scan an integer
   If the number is even - print 1, else - print 0. */
+  printf("Please enter a number:\n");
+  scanf("%d", &firstNumber);
+  //Toggle the bit at index 0, so if number was even it will be 1 and vice versa
+  firstNumber = firstNumber ^ 0x1;
+  //Print bit at index 0
+  printf("%d\n", firstNumber & 0x1);
   
   // 3, 5, 7, 11
   printf("\n3, 5, 7, 11:\n");
   /* Scan two integers in octal base
   sum them up and print the result in hexadecimal base
   Print only 4 bits, in positions: 3,5,7,11 in the result. */
+  printf("Please enter the first number (octal):\n");
+  scanf("%o", &firstNumber);
+  printf("Please enter the second number (octal):\n");
+  scanf("%o", &secondNumber);
+  //Calculate and print sum
+  result = firstNumber + secondNumber;
+  printf("The sum in hexadecimal: %X\n", result);
+  //Print bits in positions 3,5,7,11
+  printf("The 3,5,7,11 bits are: %d%d%d%d\n", (result >> 3) & 0x1, (result >> 5) & 0x1,
+    (result >> 7) & 0x1, (result >> 11) & 0x1);
 
   printf("Bye!\n");
   
